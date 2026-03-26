b4c72e701f95c8490144745e919db24d513958a9
basudevkumar <jhulonkumar40@gmail.com>
2026-03-26 16:28:48 +0600

    feat: footer bottom part

--- src/helper/imageProvider.jsx
+++ src/helper/imageProvider.jsx
@@ -1,7 +1,19 @@
 import NavImages from "../../public/images/nav-logo.png";
+import Paypal from "../../public/images/paypal.webp";
+import Amex from "../../public/images/amex.png";
+import MasterCard from "../../public/images/master-card.png";
+import Discover from "../../public/images/discover.png";
+import Apple from "../../public/images/apple-pay.png";
 
 const allImages = {
   navlogo: NavImages,
+  paymentArry: [
+    { id: 1, img: Paypal },
+    { id: 2, img: Amex },
+    { id: 3, img: MasterCard },
+    { id: 4, img: Discover },
+    { id: 5, img: Apple },
+  ],
 };
 
 export default allImages;
