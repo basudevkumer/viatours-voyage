def7a14c5c4ed16ba6731495f1e8c21b0e6978bc
basudevkumar <jhulonkumar40@gmail.com>
2026-04-21 11:05:13 +0600

    added paddingX in all devices container

--- src/components/shared/Container.jsx
+++ src/components/shared/Container.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 
 const Container = ({ children }) => {
-  return <div className="mx-auto max-w-[1320px] px-4 lg:px-0">{children}</div>;
+  return <div className="mx-auto max-w-[1320px] px-4 ">{children}</div>;
 };
 
 export default Container;
